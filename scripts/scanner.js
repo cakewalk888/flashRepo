4f7336984087eb43eeeb843af12837ac51c651e0
cakewalk888 <cakewalk888@proton.me>
2025-02-05 21:11:22 -0500

    access to Uniswap addresses for Sepolia (and the other chains)

--- scripts/scanner.js
+++ scripts/scanner.js
@@ -1,29 +1,52 @@
 // DEX scanner to find price dispraities
 
 require('dotenv').config();
-const pools = require('./pools');
+//const pools = require('./pools');
 const axios = require('axios');
 const { ethers } = require('ethers');
-const uniswap
+const { ChainId, Token, WETH,
+        Trade, Route, TokenAmount,
+        TradeType, Fetcher }
+                  = require('@uniswap/v3-sdk');
+const uniswapCore = require('@uniswap/sdk-core');
 
+//access RPC URL. I used metamask developer (infura)
+let provider = new ethers.JsonRpcProvider(process.env.SEPOLIA_RPC);
 
-let provider =
-   new ethers.providers.JsonRpcProvider(
-      process.env.SEPOLIA_RPC
-);
+async function checkRPC() {
+    try {
+        const blockNumber = await provider.getBlockNumber();
+        console.log("Latest Block:", blockNumber);
+    } catch (error) {
+        console.error("RPC Error:", error);
+    }
+}
 
-// Uniswap V2 Router & Factory Addresses
-const UNISWAP_ROUTER = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-const UNISWAP_FACTORY = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+checkRPC();
+
+// Uniswap V3 Router & Factory Addresses (Sepolia Chain)
+const chainId = new Object();
+
+chainId.sepolia = "11155111";
+
+const uniswapAddresses = uniswapCore.CHAIN_TO_ADDRESSES_MAP;
+
+const UNISWAP_ROUTER = uniswapAddresses[chainId.sepolia].quoterAddress;
+const UNISWAP_FACTORY = uniswapAddresses[chainId.sepolia].v3CoreFactoryAddress;
+
+console.log("Router/Quoter Address (Sepolia):", UNISWAP_ROUTER);
+console.log("Factory Address (Sepolia):",UNISWAP_FACTORY);
+console.log("Sepolia Uniswap Addresses:", uniswapAddresses[chainId.sepolia]);
+
 
 // Uniswap Router ABI (reduced)
 const UNISWAP_ROUTER_ABI = [
     "function getAmountsOut(uint amountIn, address[] memory path) view returns (uint[] memory amounts)"
 ];
 
-const uniswapRouter = new ethers.Contract(
+/*const uniswapRouter = new ethers.Contract(
     UNISWAP_ROUTER_ABI, provider
-);
+);*/
 
 
 
